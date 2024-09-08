f54d89ec5b3960039e4d1eba33e3ffda55e7cfa2
fduxiao <9294979+fduxiao@users.noreply.github.com>
2024-09-07 22:38:23 -0400

    separate env from build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 HERE=`dirname $0`
 HERE=`realpath $HERE`
-USER_C_MODULES=$HERE/user_mods/micropython.cmake
+source $HERE/env
 cd ../../
 echo $PWD
-make -j USER_C_MODULES="$USER_C_MODULES" BOARD=AntiGM
+make -j
