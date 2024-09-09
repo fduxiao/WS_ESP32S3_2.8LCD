63f134421ff8a6c9b39dbf6bb80c84f720f9708f
fduxiao <9294979+fduxiao@users.noreply.github.com>
2024-09-09 01:38:53 -0400

    add lvgl display driver

--- anti_gm/board.py
+++ anti_gm/board.py
@@ -2,6 +2,8 @@ import time
 from machine import *
 from .st7796 import ST7796
 from .gt911 import GT911
+from .ui import LVDispDriver
+import lvgl as lv
 
 
 class Board:
@@ -38,5 +40,23 @@ class Board:
         self.display.update()
         self.display.display_on()
 
+        lv.init()
+        self.lv_display_driver = LVDispDriver(self.scr_width, self.scr_height, blit=self.lv_blit)
+        btn = lv.btn(lv.scr_act())
+        btn.align(lv.ALIGN.CENTER, 0, 0)
+        btn.add_event(lambda e: print("touched", e), lv.EVENT.CLICKED, None)
+        label = lv.label(btn)
+        label.set_text("Hello World!")
+
     def deinit(self):
         self.display.deinit()
+        self.lv_display_driver.deinit()
+        lv.deinit()
+
+    def lv_blit(self, area, color):
+        x1 = area.x1
+        x2 = area.x2
+        y1 = area.y1
+        y2 = area.y2
+
+        self.display.block(x1, y1, x2, y2, color)
---
+++ anti_gm/ui/lv_driver.py
@@ -0,0 +1,45 @@
+import lvgl as lv
+import lv_utils
+from blob import Blob
+
+
+class LVDispDriver:
+    def __init__(self, width, height, factor=5, blit=None) -> None:
+        # initialize
+        if not lv.is_initialized():
+            lv.init()
+        # prepare the event loop
+        if not lv_utils.event_loop.is_running():
+            self.event_loop = lv_utils.event_loop()
+
+        self.blit = blit
+
+        self.disp_drv = lv.disp_create(width, height)
+
+        color_format = lv.COLOR_FORMAT.RGB565
+        self.pixel_size = lv.color_format_get_size(color_format)
+        self.disp_drv.set_color_format(color_format)
+
+        # prepare buffer
+        buf_size = width * height * self.pixel_size // factor
+        self.buf1 = Blob().malloc_dma(buf_size)
+        self.buf2 = Blob().malloc_dma(buf_size)
+        self.disp_drv.set_draw_buffers(self.buf1.mv(), self.buf2.mv(), buf_size, lv.DISP_RENDER_MODE.PARTIAL)
+
+        # self.disp_drv.set_render_mode(lv.DISPLAY_RENDER_MODE.PARTIAL)
+        self.disp_drv.set_flush_cb(self.disp_drv_flush_cb)
+
+    def disp_drv_flush_cb(self, disp_drv, area, color_p):
+        w = area.x2 - area.x1 + 1
+        h = area.y2 - area.y1 + 1
+        size = w * h
+        data_view = color_p.__dereference__(size * self.pixel_size)
+
+        # blit in background
+        if self.blit:
+            self.blit(area, data_view)
+        self.disp_drv.flush_ready()
+
+    def deinit(self):
+        self.buf1.free()
+        self.buf2.free()
